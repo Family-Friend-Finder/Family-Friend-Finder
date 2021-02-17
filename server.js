31e7892c12505287da41e89c984b8d8a54ed95f6
Val Nimirenco <valnimirenco@gmail.com>
2021-02-16 20:21:30 -0800

    react-images-uploader

--- server.js
+++ server.js
@@ -1,63 +1,70 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const session = require('express-session');
-const {v4: uuidv4} = require('uuid');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser'),
-const User = require('./models/user.js');
-const apiRoutes = require('./routes/apiRoutes');
-const mongoose = require('mongoose');;
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const User = require("./models/user.js");
+const apiRoutes = require("./routes/apiRoutes");
+const mongoose = require("mongoose");
 
 //Create Express application
 const app = express();
 
 // Connect to the database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/familyfriendfinder', {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/familyfriendfinder",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+);
 
 //to config API to use body body-parser and look for JSON in req.body
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
 app.use(cookieParser());
 
 // Configure Sessions Middleware
-app.use(session({
-  genid: function (req) {
+app.use(
+  session({
+    genid: function (req) {
       return uuidv4();
-  },
-  secret: 'SuperSecretKey',
-  resave:false,
-  saveUninitialized: true,
-  cookie: {maxAge: 60 * 60 * 1000} // 60 mins
-}));
+    },
+    secret: "SuperSecretKey",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 }, // 60 mins
+  })
+);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
- //Passport Local Strategy
- passport.use(User.createStrategy());
+//Passport Local Strategy
+passport.use(User.createStrategy());
 
- // To use with sessions
- passport.serializeUser(User.serializeUser());
- passport.deserializeUser(User.deserializeUser());
+// To use with sessions
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
- app.use(apiRoutes);
+app.use(apiRoutes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
