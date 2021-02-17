4e926e122abd3b5d2ef2f24f18ef72ccf6dc7a92
Val Nimirenco <valnimirenco@gmail.com>
2021-02-16 20:48:51 -0800

    resolved merge conflicts

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,33 +1,19 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import $ from "jquery-ajax";
-import Container from "./components/Container/index.js";
-import Navbar from "./components/Navbar/index";
-import Title from "./components/Title/index";
-import ShowFamilies from "./ShowFamilies";
-=======
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import $ from 'jquery-ajax';
 import Container from "../components/Container/index.js";
 import Navbar from "../components/Navbar/index";
 import Title from "../components/Title/index";
-import { Redirect } from 'react-router-dom';
->>>>>>> Develop
+import { Redirect } from "react-router-dom";
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
       username: "",
       password: "",
       id: "",
       isAuthenticated: false,
-=======
-      username: '', password: '', id: '', isAuthenticated: false
->>>>>>> Develop
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
@@ -38,9 +24,8 @@ class Login extends Component {
     let username = this.state.username;
     let password = this.state.password;
     $.ajax({
-<<<<<<< HEAD
       method: "POST",
-      url: `http://localhost:3000/login`,
+      url: `http://localhost:3000/api/login`,
       data: {
         username: username,
         password: password,
@@ -56,33 +41,12 @@ class Login extends Component {
       }
     );
   }
-  handleLogout() {
-    this.setState({ isAuthenticated: false, id: "" });
-  }
-=======
-      method: 'POST',
-      url: `http://localhost:3000/api/login`,
-      data: {
-        username: username,
-        password: password 
-      }
-    })
-      .then(res => {
-        console.log('res is ', res);
-        this.setState({ isAuthenticated: true, id: res._id });
-      }, err => {
-        console.log('oops!');
-        console.log(err);
-      });
-  }
 
->>>>>>> Develop
   handleUsernameChange(e) {
     this.setState({ username: e.target.value });
   }
   handlePasswordChange(e) {
     this.setState({ password: e.target.value });
-<<<<<<< HEAD
   }
   getInitialState() {
     return {
@@ -93,18 +57,6 @@ class Login extends Component {
   render() {
     if (this.state.isAuthenticated === false) {
       console.log("user is not logged in");
-=======
-  }
-  getInitialState() {
-    return {
-      isAuthenticated: false
-    }
-  }
-
-  render() {
-    if (this.state.isAuthenticated === false) {
-      console.log('user is not logged in')
->>>>>>> Develop
       return (
         <Container>
           <Title />
@@ -112,22 +64,21 @@ class Login extends Component {
           <div>
             <h1>Welcome to Family Finder!</h1>
             <form onSubmit={this.handleSubmit}>
-<<<<<<< HEAD
-              <Input
+              <input
                 type="text"
                 placeholder="username"
                 value={this.state.username}
                 onChange={this.handleUsernameChange}
               />
-              <Input
+              <input
                 type="password"
                 placeholder="password"
                 value={this.state.password}
                 onChange={this.handlePasswordChange}
               />
-              <Button type="submit" value="login">
+              <button type="submit" value="login">
                 Login
-              </Button>
+              </button>
             </form>
             <Link role="button" to="signup">
               Signup
@@ -137,50 +88,7 @@ class Login extends Component {
       );
     } else {
       console.log("user is already logged in");
-      return (
-        <Container>
-          <Title />
-          <Navbar />
-          <div>
-            <h1>Welcome to Family Finder</h1>
-            <ShowFamilies
-              isAuthenticated={this.state.isAuthenticated}
-              username={this.state.username}
-              id={this.state.id}
-            ></ShowFamilies>
-            <Button className="logout-button" onClick={this.handleLogout}>
-              Logout
-            </Button>
-          </div>
-        </Container>
-      );
-=======
-              <input
-                type='text'
-                placeholder='username'
-                value={this.state.username}
-                onChange={this.handleUsernameChange}
-              />
-              <input
-                type='password'
-                placeholder='password'
-                value={this.state.password}
-                onChange={this.handlePasswordChange}
-              />
-              <button type='submit'
-                value='login'>Login</button>
-            </form>
-            <Link role='button' to='signup'>Signup</Link>
-          </div>
-        </Container>
-
-      )
-    } else {
-      console.log('user is already logged in')
-      return (
-        <Redirect to={{ pathname: '/findfamilies' }} />
-      )
->>>>>>> Develop
+      return <Redirect to={{ pathname: "/findfamilies" }} />;
     }
   }
 }
