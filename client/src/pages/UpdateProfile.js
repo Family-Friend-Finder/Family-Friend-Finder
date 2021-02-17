81c2d7e3f8fd56e0eff0ef0167afe5d0dbd5746b
ASwartz14 <asriley144@gmail.com>
2021-02-16 21:25:46 -0800

    fixed pages to render pretty

--- client/src/pages/UpdateProfile.js
+++ client/src/pages/UpdateProfile.js
@@ -1,16 +1,21 @@
 import React from "react";
-import Container from "./components/Container/index";
-import Navbar from "./components/Navbar/index";
-import Title from "./components/Title/index";
+import Container from "../components/Container/index";
+import Navbar from "../components/Navbar/index";
+import Title from "../components/Title/index";
 import ImageUploader from "../components/ImageUploader/index";
+import Wrapper from "../components/Wrapper/index";
 
 export default function UpdateProfile(props) {
   return (
-    <Container>
+    <div>
       <Title />
+      <Container>
+        <Wrapper>
+          <h2>Lets add some color to your profile</h2>
+          <ImageUploader />
+        </Wrapper>
+      </Container>
       <Navbar />
-      <h2>Lets add some color to your profile</h2>
-      <ImageUploader />
-    </Container>
+    </div>
   );
 }
