952b7576c6cd18ed3bd09f552182dabd2b1c7748
ASwartz14 <asriley144@gmail.com>
2021-02-17 16:39:57 -0800

    fix page render

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 //import React, { Component, useState } from "react";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import $ from "jquery-ajax";
 import Container from "../components/Container/index.js";
@@ -7,7 +7,7 @@ import Navbar from "../components/Navbar/index";
 import Title from "../components/Title/index";
 // import { Redirect } from "react-router-dom";
 import Wrapper from "../components/Wrapper/index";
-import API from "../utils/API"
+import API from "../utils/API";
 import { AuthContext } from "../App";
 
 export default function Login() {
@@ -19,74 +19,100 @@ export default function Login() {
 
   const [loginState, setLoginState] = useState({
     isloggedin: false,
-    userid: ''
+    userid: "",
   });
 
-
-
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(`Username: ${username} Password: ${password}`);
 
-  $.ajax({
-    method: "POST",
-    url: `/api/login`,
-    data: {
-      username: username,
-      password: password,
-    },
-  }).then(
-    (res) => {
-      console.log("res is ", res);
-      dispatch({
-        type: "LOGIN",
-        payload: {userid: res._id}
-    });
-      setauthStatus(true);
-    },
-    (err) => {
-      console.log("oops!");
-      console.log(err);
-
-    }
-  );
-}
-
+    $.ajax({
+      method: "POST",
+      url: `/api/login`,
+      data: {
+        username: username,
+        password: password,
+      },
+    }).then(
+      (res) => {
+        console.log("res is ", res);
+        dispatch({
+          type: "LOGIN",
+          payload: { userid: res._id },
+        });
+        setauthStatus(true);
+      },
+      (err) => {
+        console.log("oops!");
+        console.log(err);
+      }
+    );
+  };
 
   console.log(`Auth Status is ${authStatus}`);
   //console.log(`${loginState}`);
 
-   return (
+  return (
     <AuthContext.Provider value={loginState}>
       <div>
         <Title />
         <Container>
-            <div>
-              <h1>Welcome to Family Finder!</h1>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="text"
-                  placeholder="username"
-                  value={username}
-                  onChange={e => setUsername(e.target.value)}
-                />
-                <input
-                  type="password"
-                  placeholder="password"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                />
-                <button type="submit" value="login">
-                  Login
-              </button>
-              </form>
-              <Link role="button" to="signup">
-                Signup
-            </Link>
-            </div>
+          <Wrapper>
+            <center>
+              <div>
+                <div className="container">
+                  <div className="row" />
+                  <div className="col-sm-1"></div>
+                  <div className="col-sm-10">
+                    <h1>Welcome to Family Finder!</h1>
+                    <br />
+                    <div className="card">
+                      <div className="card-body">
+                        <form>
+                          <h2 className="title">Login</h2>
+                          <hr />
+
+                          <div className="form-group">
+                            <label for="InputName">Name</label>
+                            <br />
+                            <input
+                              type="text"
+                              placeholder="username"
+                              value={this.state.username}
+                              onChange={this.handleUsernameChange}
+                            />
+                          </div>
+                          <div className="form-group">
+                            <label for="InputEmail">Password</label>
+                            <br />
+                            <input
+                              type="password"
+                              placeholder="password"
+                              value={this.state.password}
+                              onChange={this.handlePasswordChange}
+                            />
+                          </div>
+                          <button
+                            onClick={this.handleSubmit}
+                            type="submit"
+                            className="btn btn-info"
+                          >
+                            Login
+                          </button>
+                        </form>
+                      </div>
+                    </div>
+                    <Link role="button" to="signup">
+                      Sign-up
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </center>
+          </Wrapper>
         </Container>
         <Navbar />
       </div>
     </AuthContext.Provider>
-  ) 
-   }
+  );
+}
