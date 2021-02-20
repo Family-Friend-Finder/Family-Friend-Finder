baea3c3f9e634ea121c9de0833bb6cab676da84e
Awa <francisawa2@gmail.com>
2021-02-20 12:21:58 -0800

    updated models

--- controllers/DBRouteControllers.js
+++ controllers/DBRouteControllers.js
@@ -1,2 +1,14 @@
 import db from "../models"
 
+app.post("/api/famlies",function(req,res){   
+       
+    var mod = new model(req.body);  
+        mod.save(function(err,data){  
+            if(err){  
+                res.send(err);                
+            }  
+            else{        
+                 res.send({data:"Record has been Inserted..!!"});  
+            }  
+        }); 
+    }) 
