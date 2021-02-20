3956f3b3ca7db857105e44a91e06880c3cd2a5f6
Awa <francisawa2@gmail.com>
2021-02-20 08:44:53 -0800

    updated imgModel.js

---
+++ models/imgModels.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const ImgSchema = new Schema({
+ img: { data: Buffer, contentType: String}
+}, {
+ timestamps: true
+});module.exports = mongoose.model('Img', ImgSchema);
